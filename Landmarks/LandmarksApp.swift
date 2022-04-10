096705871ed4eb7c03fb4c3a0dc46669fc7f3655
Gustavo Figueiredo <gustavo.flcn@gmail.com>
2022-04-10 18:45:29 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Gustavo  Feliciano Figueiredo on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
